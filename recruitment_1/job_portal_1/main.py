cbed779f2103b8ff6d7b029164e4fe728619a5e0
vibedinsan <vaibhavkashyap2023@gmail.com>
2025-02-03 06:54:16 +0530

    port changed

--- recruitment_1/job_portal_1/main.py
+++ recruitment_1/job_portal_1/main.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
         db.create_all()
         print("✅ Database created successfully!")     
     #port = int(os.environ.get("PORT", 5000))  # Use Render's dynamic port
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
 
     
     
